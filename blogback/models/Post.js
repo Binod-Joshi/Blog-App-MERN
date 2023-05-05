d7f4997cb942c07eda5a74f373316719e7f7871d
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2023-05-05 20:12:11 +0530

    likes and comment in backend

--- blogback/models/Post.js
+++ blogback/models/Post.js
@@ -1,35 +1,48 @@
 const mongoose = require("mongoose");
-
 const PostSchema = new mongoose.Schema(
   {
-    title:{
-        type:String,
-        required:true,
-        unique:true,
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    desc:{
-        type:String,
-        requird:true,
+    desc: {
+      type: String,
+      requird: true,
     },
-    phot:{
-        type:String,
-        required:false,
+    phot: {
+      type: String,
+      required: false,
     },
-    userId:{
-        type:String,
-        required:true,
+    userId: {
+      type: String,
+      required: true,
     },
-    username:{
-        type:String,
-        required:true,
+    username: {
+      type: String,
+      required: true,
     },
-    categories:{
-        type:Array,
-        required:false,
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        text: String,
+        postedBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    categories: {
+      type: Array,
+      required: false,
     },
-
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Post",PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
--- blogback/routes/posts.js
+++ blogback/routes/posts.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //create post
 router.post("/", async (req, res) => {
   let newPost = new Post(req.body);
   try {
     newPost = await newPost.save();
+    // console.log(newPost);
     res.status(200).json(newPost);
   } catch (error) {
     res.status(500).json(error);
@@ -50,7 +52,7 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id).populate("comments.postedBy","_id username");
     res.status(200).json(post);
   } catch (error) {
     res.status(500).json(error);
@@ -89,5 +91,100 @@ router.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// for add likes in the post 
+router.post("/likes",async(req,res) => {
+  const {userId,postId} = req.body;
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).send({ error: 'Post not found' });
+    }
+
+    if (post.likes.includes(userId)) {
+      return res.status(400).send({ error: 'User already liked post' });
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(postId,{
+      $push:{likes:userId}
+    },
+    { new: true }
+    );
+    console.log(updatedPost)
+    res.status(200).json(updatedPost)
+  }
+   catch (error) {
+    res.status(401).send(error)
+  }
+})
+
+
+//for unlike in the post
+router.post("/unlikes",async(req,res) => {
+  const {userId,postId} = req.body;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).send({ error: 'Post not found' });
+    }
+    if (!post.likes.includes(userId)) {
+      return res.status(400).send({ error: 'User has not liked the post' });
+    }
+    const updatedPost = await Post.findByIdAndUpdate(postId,{
+      $pull:{likes:userId}
+    },
+    { new:true }
+    )
+    console.log("unlike--",updatedPost);
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+})
+
+// to add comment
+router.post("/comments",async(req,res) => {
+  const {userId,postId,text} = req.body;
+  try {
+    const comment ={
+      text:text,
+      postedBy:userId,
+    }
+    let updatedPost = await Post.findByIdAndUpdate(postId,{
+      $push:{
+        comments: {
+          $each: [comment],
+          $position: 0
+        }
+      },
+    },
+    { new:true }
+    ).populate("comments.postedBy", "_id username");
+    res.status(200).send(updatedPost);
+  } catch (error) {
+    console.status(400).log(error)
+  }
+})
+
+// to delete comment
+router.post("/deleteComment",async(req,res) => {
+  const {postId,userId,commentId} = req.body;
+  console.log(postId,userId,commentId);
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(postId,{
+      $pull:{
+        comments:{ _id: commentId } 
+      },
+    },
+    { new:true }
+    ).populate("comments.postedBy", "_id username");
+    console.log(updatedPost);
+    res.status(200).send(updatedPost);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
